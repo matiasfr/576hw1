ebc31dd409e9bc2e1cff5a4fd71ff36972a86117
matias <matiasfr@usc.edu>
2017-09-14 01:03:36 -0700

    gaussian downsampling working

--- imageReader.java
+++ imageReader.java
@@ -1,6 +1,9 @@
+import jdk.nashorn.internal.runtime.logging.DebugLogger;
+
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
+import java.nio.Buffer;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 
@@ -133,22 +136,89 @@ public class imageReader {
    }
 
     public static BufferedImage downsampleSpecific(BufferedImage inputImg, int outputWidth, int outputHeight) {
-        int inputH = inputImg.getHeight(), inputW = inputImg.getWidth();
+        int inputHeight = inputImg.getHeight(), inputWidth = inputImg.getWidth();
         BufferedImage newImg = new BufferedImage(outputWidth, outputHeight, BufferedImage.TYPE_INT_RGB);
+        float x_ratio = ((float)inputWidth)/outputWidth;
+        float y_ratio = ((float)inputHeight)/outputHeight;
         for(int x=0; x<outputWidth; x++) {
             for(int y=0;y<outputHeight;y++) {
-//                newImg.setRGB(x,y,inputImg.getRGB(x*outputWidth/inputW,y*outputHeight/inputH) );
-                newImg.setRGB(x,y,inputImg.getRGB(x,y) );
 
+                newImg.setRGB(x,y,inputImg.getRGB((int)(x*x_ratio),(int)(y*y_ratio)) );
             }
         }
 
        return newImg;
     }
+    public static int getNeighborColorAverage(BufferedImage img, int x, int y) {
+        int redCount = 0;
+        int greenCount = 0;
+        int blueCount = 0;
+
+        Color center = new Color(img.getRGB(x, y));
+        redCount += center.getRed();
+        greenCount += center.getGreen();
+        blueCount += center.getBlue();
+
+        //get up
+        Color up = new Color(img.getRGB(x, y));
+        if (y > 0) {
+            up = new Color(img.getRGB(x, y-1));
+        }
+        redCount += up.getRed();
+        greenCount += up.getGreen();
+        blueCount += up.getBlue();
+
+        //get down
+        Color down = new Color(img.getRGB(x, y));
+        if (y+1 < img.getHeight()) {
+            down = new Color(img.getRGB(x, y+1));
+        }
+        redCount += down.getRed();
+        greenCount += down.getGreen();
+        blueCount += down.getBlue();
+
+        //get left
+        Color left = new Color(img.getRGB(x, y));
+        if (x > 0) {
+            left = new Color(img.getRGB(x-1, y));
+        }
+        redCount += left.getRed();
+        greenCount += left.getGreen();
+        blueCount += left.getBlue();
+
+        //get right
+        Color right = new Color(img.getRGB(x, y));
+        if (x+1 < img.getWidth()) {
+            right = new Color(img.getRGB(x+1, y));
+        }
+        redCount += right.getRed();
+        greenCount += right.getGreen();
+        blueCount += right.getBlue();
+
+        Color avg = new Color(redCount/5,greenCount/5,blueCount/5);
+        return avg.getRGB();
+    }
 
     public static BufferedImage downsampleGaussian(BufferedImage inputImg, int outputWidth, int outputHeight) {
-        int inputH = inputImg.getHeight(), inputW = inputImg.getWidth();
-        return inputImg;
+        int inputHeight = inputImg.getHeight(), inputWidth = inputImg.getWidth();
+        BufferedImage newImg = new BufferedImage(outputWidth, outputHeight, BufferedImage.TYPE_INT_RGB);
+
+        float x_ratio = ((float)inputWidth)/outputWidth;
+        float y_ratio = ((float)inputHeight)/outputHeight;
+
+        for(int x=0; x<outputWidth; x++) {
+            for(int y=0;y<outputHeight;y++) {
+
+                int avgColor =  getNeighborColorAverage(inputImg, (int)(x*x_ratio),(int)(y*y_ratio) );
+
+                //after we've avergaed the colors, scale the result to the proper place
+                newImg.setRGB(x,y,avgColor);
+
+
+            }
+        }
+
+        return newImg;
     }
 
     public static BufferedImage upsampleNearestNeighbor(BufferedImage inputImg, int outputWidth, int outputHeight) {
@@ -159,7 +229,42 @@ public class imageReader {
     }
 
     public static BufferedImage upsampleCubic(BufferedImage inputImg, int outputWidth, int outputHeight) {
-        return inputImg;
+        int inputHeight = inputImg.getHeight(), inputWidth = inputImg.getWidth();
+
+        BufferedImage newImg = new BufferedImage(outputWidth, outputHeight, BufferedImage.TYPE_INT_RGB);
+        float x_ratio = ((float)inputWidth-1)/outputWidth;
+        float y_ratio = ((float)inputHeight-1)/outputHeight;
+
+        int A, B, C, D, x, y, index, gray ;
+
+
+        float x_diff, y_diff, ya, yb ;
+        int offset = 0 ;
+        for (int i=0;i<outputHeight;i++) {
+            for (int j = 0; j < outputWidth; j++) {
+                x = (int) (x_ratio * j);
+                y = (int) (y_ratio * i);
+                x_diff = (x_ratio * j) - x;
+                y_diff = (y_ratio * i) - y;
+                index = y * inputWidth + x;
+
+                // range is 0 to 255 thus bitwise AND with 0xff
+                A = inputImg.getRGB(x, y) & 0xff;
+                B = inputImg.getRGB(x + 1, y) & 0xff;
+                C = inputImg.getRGB(x, y + 1) & 0xff;
+                D = inputImg.getRGB(x + 1, y + 1) & 0xff;
+
+                // Y = A(1-w)(1-h) + B(w)(1-h) + C(h)(1-w) + Dwh
+                gray = (int) (
+                        A * (1 - x_diff) * (1 - y_diff) + B * (x_diff) * (1 - y_diff) +
+                                C * (y_diff) * (1 - x_diff) + D * (x_diff * y_diff)
+                );
+
+                newImg.setRGB(x,y,gray);
+            }
+        }
+
+        return newImg;
     }
 
 
